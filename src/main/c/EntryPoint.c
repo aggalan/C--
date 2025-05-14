f533753cad8f637e3d4f988f2adc6b65f98f8510
aggalan <aggalan@itba.edu.ar>
2025-05-14 18:49:44 -0300

    undo last commit

--- src/main/c/EntryPoint.c
+++ src/main/c/EntryPoint.c
@@ -41,7 +41,7 @@ const int main(const int count, const char ** arguments) {
 		// Beginning of the Backend... ------------------------------------------------------------
 		logDebugging(logger, "Computing expression value...");
 		Program * program = compilerState.abstractSyntaxtTree;
-		ComputationResult computationResult = computeExpression(program->statements);
+		ComputationResult computationResult = computeExpression(program->expression);
 		if (computationResult.succeed) {
 			compilerState.value = computationResult.value;
 			generate(&compilerState);
